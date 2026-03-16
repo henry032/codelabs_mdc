fb1b9187388b6e7ae33f6398e088edfbbf9453bb
Henry Cabrera <hsalguero183@gmail.com>
2026-03-15 19:10:21 -0600

    paso 7

--- mdc_100_series/lib/app.dart
+++ mdc_100_series/lib/app.dart
@@ -17,6 +17,7 @@ import 'package:flutter/material.dart';
 import 'home.dart';
 import 'login.dart';
 import 'colors.dart';
+import 'supplemental/cut_corners_border.dart';
 
 // TODO: Convert ShrineApp to stateful widget (104)
 class ShrineApp extends StatelessWidget {
@@ -61,8 +62,8 @@ ThemeData _buildShrineTheme() {
     // TODO: Add the icon themes (103)
     // TODO: Decorate the inputs (103)
     inputDecorationTheme: const InputDecorationTheme(
-      border: OutlineInputBorder(),
-      focusedBorder: OutlineInputBorder(
+      border: CutCornersBorder(),
+      focusedBorder: CutCornersBorder(
         borderSide: BorderSide(
           width: 2.0,
           color: kShrineBrown900,
--- mdc_100_series/lib/login.dart
+++ mdc_100_series/lib/login.dart
@@ -78,6 +78,9 @@ class _LoginPageState extends State<LoginPage> {
                   },
                   style: TextButton.styleFrom(
                     foregroundColor: Theme.of(context).colorScheme.secondary,
+                    shape: const BeveledRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(7.0)),
+                    ),
                   ),
                 ),
                 // TODO: Add an elevation to NEXT (103)
@@ -92,6 +95,9 @@ class _LoginPageState extends State<LoginPage> {
                     foregroundColor: Colors.black,
                     backgroundColor: Theme.of(context).colorScheme.primary,
                     elevation: 8.0,
+                    shape: const BeveledRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(7.0)),
+                    ),
                   ),
                 ),
               ],
